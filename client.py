a3bf0605e8376a6677f7e7ac15a0d7af15a97516
John Wallis <eviper01@gmail.com>
2019-11-24 11:02:21 +0800

    Draft Build 1.0.1
    Client now receives requests from server on what to dispatch
    Bank Transfers now avalaible

--- client.py
+++ client.py
@@ -1,60 +1,32 @@
 import socketio
-index_dispatcher = None
-print("imported")
-io = socketio.Client()
-@io.event
-def connect():
-    print('**connection established**')
-    io.emit("new_player")
-    while True:
-        command = Get_input()
+try:
+    print("imported")
+    io = socketio.Client()
+    @io.event
+    def connect():
+        print('**connection established**')
+        io.emit("new_player")
+    @io.event
+    def disconnect():
+        print('disconnected from server')
+        exit()
+    @io.event
+    def broadcast(info):
+        print(info)
+    @io.event
+    def Get_Input(dispatcher):
+        command = input()
+        instruction = command.partition(' ')[0]
         try:
-            io.emit(command[0], {"argument":command[1]})
-        except:
-            pass
-@io.event
-def disconnect():
-    print('disconnected from server')
-    exit()
-@io.event
-def broadcast(info):
-    print(info)
-@io.event
-def inputer(dispatcher):
-    global index_dispatcher
-    index_dispatcher = dispatcher
-
-#this is probably better done with a dispatcher = {list of comands}
-
-def Get_input():
-    global index_dispatcher
-    dispatcher = {  "help":"help",
-                    "logs":"logs",
-                    "ip":"ip",
-                    "connect":"connect",
-                    "disconnect":"disconnect",
-                    "clearlogs":"clear_logs",
-                    "editlogs": "edit_logs",
-                    "index":"index",
-                    "run":"run",
-                    "data":"control_data"}
-    command = input()
-    instruction = command.partition(' ')[0]
-    try:
-        argument = command.partition(' ')[2]
-    except IndexError:
-        argument = 0
-    if index_dispatcher != None:
+            argument = command.partition(' ')[2]
+        except IndexError:
+            argument = 0
         try:
-            io.emit(index_dispatcher[instruction], {"argument":argument})
+            io.emit(dispatcher[instruction], {"argument":argument})
         except KeyError:
-            print("Invalid Request, Exiting Index")
-        index_dispatcher = None
-    else:
-        try:
-            return [dispatcher[instruction], argument]
-        except KeyError:
-            print("Unknown Command")
-            return Get_input()
+            print("Invalid Input")
+            Get_Input(dispatcher)
 
-io.connect("http://localhost:25444")
+    io.connect("http://localhost:25444")
+except:
+    io.disconnect()
--- server.js
+++ server.js
@@ -6,17 +6,28 @@ var io = require('socket.io')(server);
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
+
 server.listen(25444, function () {
 
   init_database();
   Create_AI("whois");
   Create_AI("Contract-Broker");
   generate_bank()
-  data.players["Contract-Broker"].index = function() {return ["Currently we dont offer contracts", null]}
-  data.players.whois.index = function() {return ["Welcome to the Whois, Registered ip:\n CommSecAuto-Banking \n Contract-Broker" , null]}
+  data.players["Contract-Broker"].index = function() {return ["Currently we dont offer contracts", global_dispatcher]}
+  data.players.whois.index = function() {return ["Welcome to the Whois, Registered ip:\n CommSecAuto-Banking \n Contract-Broker" , global_dispatcher]}
   console.log("server running on 25444");
 });
-//backend init
+//backend init - note dispatcher system could be used to send custom errors
+var global_dispatcher = {  "help":"help",
+                            "logs":"logs",
+                            "ip":"ip",
+                            "connect":"connect",
+                            "disconnect":"disconnect",
+                            "clearlogs":"clear_logs",
+                            "editlogs": "edit_logs",
+                            "index":"index",
+                            "run":"run",
+                            "data":"control_data"}
 var data = {};
 function init_database() {
   data["players"] = {};
@@ -51,7 +62,7 @@ function Create_AI(ip) {
 
 function generate_bank() {
   Create_AI("CommSecAuto-Banking");
-  data.players["CommSecAuto-Banking"].index = function() {return ["Welcome To CommSec: \nlogin\nregister ", {help:"bank_help", login:"bank_login", register:"bank_register"}]}
+  data.players["CommSecAuto-Banking"].index = function() {return ["Welcome To CommSec: \nlogin\nregister ", {help:"bank_help", login:"bank_login", register:"bank_register","cancel":"global_cancel"}]}
 
 }
 
@@ -131,42 +142,51 @@ io.on('connect', function(socket) {
     };
     data.players[socket.id] = player;
     console.log(data);
+    socket.emit("Get_Input",global_dispatcher);
   });
 
   socket.on('help', function(input){
     var info = "Git Good";
     socket.emit("broadcast", info);
+    socket.emit("Get_Input",global_dispatcher);
   });
   socket.on('logs', function(input){
     socket.emit("broadcast", "Printing Logs...");
     socket.emit("broadcast", data.players[data.players[socket.id].connection].print_log());
+    socket.emit("Get_Input",global_dispatcher);
   });
   socket.on('clear_logs', function(input){
     socket.emit("broadcast", "Clearing Logs");
     setTimeout(function() {data.players[data.players[socket.id].connection].clear_log();
-                          socket.emit("broadcast", "success");},1500);
+                          socket.emit("broadcast", "success");
+                          socket.emit("Get_Input",global_dispatcher);},1500);
+
 
   });
   socket.on('edit_logs', function(input){
     socket.emit("broadcast", "Editing Logs...");
     setTimeout(function() {data.players[socket.id].edit_log(input.argument);
-                          socket.emit("broadcast", "success");},3000);
+                          socket.emit("broadcast", "success");
+                          socket.emit("Get_Input",global_dispatcher);},3000);
 
   });
   socket.on('connect', function(input){
     try {
-    socket.emit("broadcast", "Connecting to: " + input.argument);
+    socket.emit("broadcast", "Connected to: " + input.argument);
     data.players[socket.id].connect(input.argument);
-    data.players[socket.id].log.push("connected to: " + data.players[socket.id].connection + " at " + new Date().toISOString());}
+    data.players[socket.id].log.push("connected to: " + data.players[socket.id].connection + " at " + new Date().toISOString());
+    socket.emit("Get_Input",global_dispatcher);}
     catch {
       socket.emit("broadcast", "404 Error: Server did not respond");
       data.players[socket.id].connection = data.players[socket.id].ip;
+      socket.emit("Get_Input",global_dispatcher);
     }
   });
   socket.on('disconnect', function(input){
     socket.emit("broadcast", "Disconnecting...");
     setTimeout(function() {data.players[socket.id].disconnect();
-                          socket.emit("broadcast", "Diconnected")},1500);
+                          socket.emit("broadcast", "Diconnected")
+                          socket.emit("Get_Input",global_dispatcher);},1500);
 
   });
 
@@ -174,22 +194,34 @@ io.on('connect', function(socket) {
     try {
     page_file = data.players[data.players[socket.id].connection].index();
     socket.emit("broadcast", page_file[0]);
-    socket.emit("inputer",page_file[1])}
+    socket.emit("Get_Input",page_file[1]);}
     catch {
-      socket.emit("broadcast", "404 Error: Server rejected connection");}
+      socket.emit("broadcast", "404 Error: Server rejected connection");
+      socket.emit("Get_Input",global_dispatcher);}
   });
   socket.on('ip', function(input){
     socket.emit("broadcast", data.players[socket.id].connection + " as " + data.players[socket.id].permission);
+    socket.emit("Get_Input",global_dispatcher);
   });
   socket.on('run', function(input){
     try {
-    data.players[socket.id].files[input.argument](socket.id);}
+      //this might need to be reenginnered
+    data.players[socket.id].files[input.argument](socket.id);
+    socket.emit("Get_Input",global_dispatcher);} ///this input request is a little bit wacky
      catch {
       socket.emit("broadcast","Unknown Program");
+      socket.emit("Get_Input",global_dispatcher);
    }
   });
 
 //specific functions for certain AI
+// necessary to prevent soft loops
+socket.on("global_cancel", function(input){
+  socket.emit("broadcast", "Operation Canceled");
+  socket.emit("Get_Input", global_dispatcher);
+});
+
+
 //Banking Functions
 
   socket.on("bank_login", function(input) {
@@ -199,24 +231,47 @@ io.on('connect', function(socket) {
     socket.emit("broadcast", "account has balance: $" + data.global.bankaccounts[input.argument])
     data.players["CommSecAuto-Banking"].log.push(socket.id + " logged into account: " + input.argument)
     data.players[socket.id].log.push(socket.id + " logged into account: " + input.argument)
+    socket.emit("broadcast", "Type transfer and an account number to initiate transfer")
+    socket.emit("Get_Input", {"transfer":"bank_transfer","cancel":"global_cancel"});
 
   }
   else {
-    socket.emit("broadcast", "invalid account number")
+    socket.emit("broadcast", "invalid account number");
+    socket.emit("Get_Input",{help:"bank_help", login:"bank_login", register:"bank_register","cancel":"global_cancel"});
+  }
+  });
+  socket.on("bank_transfer", function(input) {
+    if (Object.keys(data.global.bankaccounts).includes(input.argument)) {
+    var transfer_amount = data.global.bankaccounts[data.players[socket.id].logged_bank_account]
+    data.global.bankaccounts[input.argument] += data.global.bankaccounts[data.players[socket.id].logged_bank_account]
+    socket.emit("broadcast","Transfered $" + transfer_amount + " from " + data.players[socket.id].logged_bank_account + " to " + input.argument)
+    data.players["CommSecAuto-Banking"].log.push(socket.id + " Transfered $" + transfer_amount + " from " + data.players[socket.id].logged_bank_account + " to " + input.argument)
+    data.players[socket.id].log.push("Transfered $" + transfer_amount + " from " + data.players[socket.id].logged_bank_account + " to " + input.argument)
+    data.global.bankaccounts[data.players[socket.id].logged_bank_account] = 0
+    socket.emit("Get_Input", global_dispatcher);
+  }
+  else {
+    socket.emit("broadcast", "invalid account number: try a different account number");
+    socket.emit("Get_Input", {"transfer":"bank_transfer","cancel":"global_cancel"});
   }
   });
   socket.on("bank_help", function() {
     socket.emit("broadcast", "\nlogin\nregister")
+    page_file = data.players[data.players[socket.id].connection].index();
+    socket.emit("broadcast", page_file[0]);
+    socket.emit("Get_Input",page_file[1]);
   });
   socket.on("bank_register", function() {
     account_name = Math.floor(Math.random()*100000000);
     data.global.bankaccounts[account_name] = 0;
-    socket.emit("broadcast","created account: " + account_name + "\nit is highly recommended that you save this number keep it private")
+    socket.emit("broadcast","created account: " + account_name + "\nit is highly recommended that you save this number keep it private");
+    socket.emit("Get_Input",global_dispatcher);
   });
 //server control functions - this probably needs some kind of authentication or something cause otherwise this might be the big bad
 //also once functions can occure once per contorl connection which might do some weird shit --> yep it does do wierd shit so this could be problematic
   socket.on('control_data', function(input){
     console.log(data);
+    socket.emit("Get_Input",global_dispatcher);
   });
 
 });
